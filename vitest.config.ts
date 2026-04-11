0a5b52361d928de1d221e466191d7010c7d6ceee
Renato-Cesarr <renatocesaroficial01@gmail.com>
2026-04-11 16:21:58 -0300

    ci: centralizando config do sonar e refinando vitest para lidar com coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,11 +2,14 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    globals: true,
+    environment: 'jsdom',
     coverage: {
-      enabled: true,
       provider: 'v8',
       reporter: ['lcov', 'text'],
-      reportsDirectory: './coverage'
+      reportsDirectory: './coverage',
+      include: ['src/**/*.ts'],
+      exclude: ['src/**/*.spec.ts', 'src/main.ts', 'src/environments/**']
     },
   },
 });
